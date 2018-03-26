38212096c8241ea17a7e6a8ae194d88338869274
Griffin Schneider <griffin@gasbuddy.com>
2018-03-26 20:50:29 -0300

    Better testbed with Alamofire.

---
+++ Testbed/Testbed/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Testbed
+//
+//  Created by Griffin Schneider on 3/26/18.
+//  Copyright © 2018 Griffin Schneider. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
